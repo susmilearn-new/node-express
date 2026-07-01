65d7abe1e1126807397cee7fff5b8e9e1b8d09f9
susmi. learn <susmi.learn@gmail.com>
2026-07-01 21:29:20 +0530

    express push

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const express =  require('express')
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Home page")
+})
+
+app.get("/about", (req,res)=>{
+    res.send("About page")
+})
+
+app.get("/service", (req,res)=>{
+    res.send("Service page")
+})
+
+app.listen(5000, ()=>{
+    console.log("Server Running..")
+})
