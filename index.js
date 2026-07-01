0f1a3c62acbef45ac5f25bb738753013be2aec39
susmi. learn <susmi.learn@gmail.com>
2026-07-01 21:53:19 +0530

    json format

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.get("/service", (req,res)=>{
     res.send("Service page")
 })
 
+app.get("/users",(req,res)=>{
+    res.json(
+        {
+            "name": "Arjun",
+            "Age": 26
+        }
+    )
+})
+
 app.listen(5000, ()=>{
     console.log("Server Running..")
 })
